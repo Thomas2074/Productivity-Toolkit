7ccb0ee8f2b8426e3deebbaa692b7cf69c6ce016
Thomas Crutchfield Jr. <77126818+Thomas2074@users.noreply.github.com>
2026-06-30 22:41:14 -0400

    Fix author comments formatting in user script

--- productivity_toolkit.user.js
+++ productivity_toolkit.user.js
@@ -4,8 +4,8 @@
 // @version      0.2.8
 // @description  Local-first browser productivity suite with notes, snippets, focus blocking, timer, reports, highlights, shortcuts, site rules, and backup/restore.
 // @author       Thomas Crutchfield
-// @author GitHub: https://github.com/Thomas2074
-// @author LinkedIn: https://www.linkedin.com/in/thomas-crutchfield-780957192/
+// @author       GitHub: https://github.com/Thomas2074
+// @author       LinkedIn: https://www.linkedin.com/in/thomas-crutchfield-780957192/
 // @match        http://*/*
 // @match        https://*/*
 // @grant        GM_getValue
